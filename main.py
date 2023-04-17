5e202b6d1d4e18551762a22465721e3f26fa5eb9
portasorta <portasorta@gmail.com>
2023-04-17 14:10:26 +0300

    delete text

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class game:
 game_sl1 = game('Ninja Gaiden 2', 2009, 'xbox360')
 game_sl2 = game('Ninja Gaiden 3', 2012, 'many')
 
-
 class game2(game):
 
     def __init__(self, name, year, platform, platform2):
@@ -24,12 +23,9 @@ class game2(game):
     def run(self):
         return 'I can play on ps3 and xbox360'
 
-
 game_sl3 = game2('Ninja Gaiden 3', 2012, 'PS3', 'xbox360')
 print(game_sl3.getname())
 print(game_sl3.type_class)
 print(game_sl3.run())
-
-
 print(game_sl3.__dict__)
 
